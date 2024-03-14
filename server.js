2a598ca9b8bfdb110d925a380534f901839760ba
ivxni <ivxnv@outlook.com>
2024-03-14 20:25:22 +0100

    cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const PORT = 3001; // Verwende einen anderen Port als das React-Frontend
+const PORT = 3001;
 
-app.use(express.json()); // Middleware, um JSON-Anfragen zu verarbeiten
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hallo von meinem Backend!' });
+app.get("/", (req, res) => {
+  res.json({ message: "Hallo von meinem Backend!" });
 });
 
 app.listen(PORT, () => {
